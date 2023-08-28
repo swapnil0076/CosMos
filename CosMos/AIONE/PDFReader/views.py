46f01b6679aa6264d9f24412c04b4c9976053aba
swapnil0076 <swapnildhiman910@gmail.com>
2023-08-29 01:05:38 +0530

    Module-1 AND Module 02

--- CosMos/AIONE/PDFReader/views.py
+++ CosMos/AIONE/PDFReader/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+import openai
+import chromadb
+import os
+from langchain.document_loaders import TextLoader
+from langchain.indexes import VectorstoreIndexCreator
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+loader = TextLoader("Data\info.txt")
+index = VectorstoreIndexCreator().from_loaders([loader])
+print(index.query("Tell me something about Harpreet’s family?"))
